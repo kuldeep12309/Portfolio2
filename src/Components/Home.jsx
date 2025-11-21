d7af9093cb5ab04d334ffc43c4bbc724f765aec6
kuldeep gupta <kuldeepgupta8800@gmail.com>
2025-11-21 15:28:21 +0530

    some changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,11 +2,11 @@ import { useEffect, useRef } from "react";
 import Typed from "typed.js";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-
 import About from "./About";
 import Resume from "./Resume";
 import Contact from "./Contact";
 import Project from "./Project";
+import kuldeep from "../assets/kuldeep.jpg";
 
 const Home = () => {
   const typedEl = useRef(null);
@@ -29,9 +29,7 @@ const Home = () => {
 
   return (
     <div className="scroll-smooth">
-      {/* HEADER */}
-      <section className="flex flex-col md:flex-row items-center justify-center  py-10">
-        {/* LEFT */}
+      <section className="flex flex-col md:flex-row items-center justify-center py-10">
         <motion.div
           className="w-full md:w-1/2 p-5 text-center md:text-left"
           initial={{ opacity: 0, x: -100 }}
@@ -39,7 +37,7 @@ const Home = () => {
           transition={{ duration: 1.2, ease: "easeOut" }}
         >
           <h1
-            className="text-2xl md:text-4xl font-bold "
+            className="text-2xl md:text-4xl font-bold"
             style={{ fontFamily: "asif" }}
           >
             Hi, I'm{" "}
@@ -48,23 +46,22 @@ const Home = () => {
             </span>
           </h1>
 
-          <p className="text-gray-600 mt-6 text-lg ">
+          <p className="text-gray-600 mt-6 text-lg">
             I’m a full-stack web developer passionate about building powerful,
             scalable, and elegant web solutions using{" "}
-            <span className="font-semibold ">React</span> on the frontend and{" "}
+            <span className="font-semibold">React</span> on the frontend and{" "}
             <span className="font-semibold text-green-600">Django</span> on the
             backend. I craft seamless user experiences powered by APIs.
           </p>
 
           <Link
             to="/resume"
-            className="inline-block mt-6 px-6 py-3  shadow-lg   rounded-md hover:bg-blue-300 hover:text-black transition duration-300 "
+            className="inline-block mt-6 px-6 py-3 shadow-lg rounded-md hover:bg-blue-300 hover:text-black transition duration-300"
           >
             View Resume
           </Link>
         </motion.div>
 
-        {/* RIGHT */}
         <motion.div
           className="w-full md:w-1/2 text-center p-5 flex flex-col items-center"
           initial={{ opacity: 0, x: 100 }}
@@ -72,19 +69,19 @@ const Home = () => {
           transition={{ duration: 1.2, ease: "easeOut" }}
         >
           <motion.img
-            src="https://www.w3schools.com/howto/img_avatar.png"
-            alt="Full-Stack Developer"
-            className="w-1/2 rounded-full shadow-2xl border-4 border-blue-200 hover:scale-105 transition-transform duration-500"
-            whileHover={{ scale: 1.05, rotate: 2 }}
+            src={kuldeep}
+            alt="Kuldeep Gupta"
+            className="w-76 h-76  rounded-full object-cover hover:scale-110 transition-transform duration-300"
           />
+
           <h4 className="m-4 text-green-600 italic text-2xl md:text-3xl font-semibold">
             Kuldeep Gupta
           </h4>
+
           <p className="text-gray-500">React , Django , Django Rest , MySQL</p>
         </motion.div>
       </section>
 
-      {/* SECTIONS */}
       <hr className="my-10 border-gray-300" />
 
       <motion.div
@@ -117,6 +114,7 @@ const Home = () => {
       >
         <Resume />
       </motion.div>
+
       <hr className="my-10 border-gray-300" />
 
       <motion.div
